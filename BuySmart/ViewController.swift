48a2ecc90395e3daab1338f308035ca269d6ad3c
ulfaslak <ulfjensen@gmail.com>
2015-04-15 16:27:59 +0200

    first commit

---
+++ BuySmart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BuySmart
+//
+//  Created by Ulf Aslak Jensen on 15/04/15.
+//  Copyright (c) 2015 Ulf Aslak Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lol: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lol.text = "lol"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
